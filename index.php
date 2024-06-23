4b7311b101e0841126c789b7376d338bbcf51838
miray-mustafov <mustafovmiray@gmail.com>
2024-06-23 21:51:41 +0300

    match ()

--- index.php
+++ index.php
@@ -10,24 +10,34 @@
 
 <body>
 
-    <main>
-        <form action="includes/formhandler.php" method="post">
-            <label for="firstname">Firstname</label>
-            <input required id="firstname" type="text" name='firstname' placeholder=Firstname...>
-            
-            <label required for="lastname">Lastname</label>
-            <input id="lastname" type="text" name='lastname' placeholder=Lastname...>
+<main>
+    <?php
+    $a = 123;
+    $res = match ($a) {
+        2 => 'variable is 2',
+        1 => 'variable is 1',
+        default => "variable is $a",
+    };
+    echo $res
+    ?>
 
-            <label for="favouritepet">Favourite Pet</label>
-            <select name="favouritepet" id="favouritepet">
-                <option value="none">None</option>
-                <option value="dog">Dog</option>
-                <option value="cat">Cat</option>
-            </select>
+    <form action="includes/formhandler.php" method="post">
+        <label for="firstname">Firstname</label>
+        <input required id="firstname" type="text" name='firstname' placeholder=Firstname...>
 
-            <button type="submit">Submit</button>
-        </form>
-    </main>
+        <label required for="lastname">Lastname</label>
+        <input id="lastname" type="text" name='lastname' placeholder=Lastname...>
+
+        <label for="favouritepet">Favourite Pet</label>
+        <select name="favouritepet" id="favouritepet">
+            <option value="none">None</option>
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+        </select>
+
+        <button type="submit">Submit</button>
+    </form>
+</main>
 
 </body>
 
