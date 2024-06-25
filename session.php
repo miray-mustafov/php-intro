ec7bbb2038d08afb8e58a7b5b83814ad055e618d
miray-mustafov <mustafovmiray@gmail.com>
2024-06-25 21:22:29 +0300

    session php

---
+++ session.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+$_SESSION["username"] = "miralcho"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h3>Session</h3>
+<?php
+
+?>
+</form>
+</body>
+</html>
